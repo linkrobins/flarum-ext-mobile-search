8073791046e79bf42bd1a78526be6222384d9e2e
Link Robins <karl@linkrobins.com>
2026-02-21 17:32:17 -0500

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -3,11 +3,11 @@ import app from 'flarum/forum/app';
 import IndexPage from 'flarum/forum/components/IndexPage';
 import Search from 'flarum/forum/components/Search';
 
-// IMPORTANT: This ID must match your composer.json name (linkrobins-mobile-search)
 app.initializers.add('linkrobins-mobile-search', () => {
-    extend(IndexPage.prototype, 'viewItems', function (items) {
-        // We use viewItems because you said it worked before
-        // 100 priority forces it to the top of the list
+    console.log('Mobile search extension is running'); // This will help us debug
+
+    extend(IndexPage.prototype, 'navItems', function (items) {
+        // We add a custom class 'MobileSearch' to bypass Flarum's default hidden search CSS
         items.add('search', Search.component({
             state: app.search,
         }), 100);
