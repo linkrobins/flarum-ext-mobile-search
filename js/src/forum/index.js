9428e63da9fb1b7bb96ffcaa532aca7e77510886
Link Robins <karl@linkrobins.com>
2026-02-21 18:02:05 -0500

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,8 +5,9 @@ import Search from 'flarum/forum/components/Search';
 
 app.initializers.add('linkrobins-mobile-search', () => {
     extend(IndexPage.prototype, 'viewItems', function (items) {
+        // -10 priority puts it exactly between the sort dropdown and the action buttons
         items.add('search', Search.component({
             state: app.search,
-        }), -10); // Priority -10 puts it after the Sort dropdown
+        }), -10);
     });
 });
