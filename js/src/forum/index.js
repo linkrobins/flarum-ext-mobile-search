971bc103d42c5bb6d445b209a58c318916c67eca
Link Robins <karl@linkrobins.com>
2026-02-21 17:10:08 -0500

    Create index.js

---
+++ js/src/forum/index.js
@@ -0,0 +1,12 @@
+import { extend } from 'flarum/common/extend';
+import app from 'flarum/forum/app';
+import IndexPage from 'flarum/forum/components/IndexPage';
+import Search from 'flarum/forum/components/Search';
+
+app.initializers.add('linkrobins-mobile-search', () => {
+    extend(IndexPage.prototype, 'viewItems', function (items) {
+        items.add('search', Search.component({
+            state: app.search,
+        }), -100);
+    });
+});
