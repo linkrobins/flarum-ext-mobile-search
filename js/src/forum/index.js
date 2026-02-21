8e0c48b4afdd4d5d070a18913e901481358079af
Link Robins <karl@linkrobins.com>
2026-02-21 18:17:28 -0500

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,13 +1,12 @@
-import { extend } from 'flarum/common/extend';
-import app from 'flarum/forum/app';
-import IndexPage from 'flarum/forum/components/IndexPage';
-import Search from 'flarum/forum/components/Search';
+import {extend} from 'flarum/extend';
+import app from 'flarum/app';
+import IndexPage from 'flarum/components/IndexPage';
+import Search from 'flarum/components/Search';
 
-app.initializers.add('linkrobins-mobile-search', () => {
+app.initializers.add('clarkwinkelmann-mobile-search', () => {
     extend(IndexPage.prototype, 'viewItems', function (items) {
-        // Using -10 priority puts it in the middle of the toolbar
         items.add('search', Search.component({
             state: app.search,
-        }), -10);
+        }), -100);
     });
 });
