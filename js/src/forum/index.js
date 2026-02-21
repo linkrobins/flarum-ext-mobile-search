b5bcb1e6f890f8d9928a923ed49fa1afeb37eb5f
Link Robins <karl@linkrobins.com>
2026-02-21 17:23:59 -0500

    Update index.js

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -4,9 +4,12 @@ import IndexPage from 'flarum/forum/components/IndexPage';
 import Search from 'flarum/forum/components/Search';
 
 app.initializers.add('linkrobins-mobile-search', () => {
-    extend(IndexPage.prototype, 'viewItems', function (items) {
+    // We use sidebarItems because they are visible at the top of the mobile home page
+    extend(IndexPage.prototype, 'sidebarItems', function (items) {
+        
+        // Only show this on the homepage (IndexPage)
         items.add('search', Search.component({
             state: app.search,
-        }), -100);
+        }), 100); // Higher priority (100) moves it to the very top
     });
 });
